4025558ccb4492ce7c523952a816b33ef3529675
Andre Powers <andre@stuypend.com>
2021-02-09 16:44:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,11 +8,12 @@ def key_for_min_value(name_hash)
   else
     min = name_hash[0]
     name_hash.each do |key, value|
-      min = value < min ? value : min
+      min = value < min ? key : min
     end
   end
   return min
 
 end
+
 ikea = {:chair => 25, :table => 85, :mattress => 450}
-puts "Here lies #{ikea[0][0]}"
+puts "Here lies #{ikea[:chair]}"
