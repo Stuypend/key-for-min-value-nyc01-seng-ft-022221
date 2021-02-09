d3930582c2a7bb64c5706d85f95b1eb8500584b5
Andre Powers <andre@stuypend.com>
2021-02-09 16:49:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -19,5 +19,5 @@ def key_for_min_value(name_hash)
 
 end
 
-ikea = {:chair => 25, :table => 85, :mattress => 450}
+ikea = {:chair => 2255, :table => 85, :mattress => 450}
 puts key_for_min_value(ikea)
