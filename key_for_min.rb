48624c0ac365f1915e6f99290ad921d8742f25eb
Andre Powers <andre@stuypend.com>
2021-02-09 16:49:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,15 @@ def key_for_min_value(name_hash)
     return nil
   else
     min = name_hash.first[1]
+    minkey = name_hash.first[0]
     name_hash.each do |key, value|
-      min = value < min ? key : min
+      if (value < min) 
+        minkey = key
+        min = value
+      end
     end
   end
-  return min
+  return minkey
 
 end
 
