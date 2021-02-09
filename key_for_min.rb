a6b4e4cec56b9f7490444caa120e7845d8b12c16
Andre Powers <andre@stuypend.com>
2021-02-09 16:51:38 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,18 @@
 
 def key_for_min_value(name_hash)
 
+  if (name_hash == nil || name_hash == {})
+    return nil
+  else
+    min = name_hash.first[1]
+    minkey = name_hash.first[0]
+    name_hash.each do |key, value|
+      if (value < min)
+        minkey = key
+        min = value
+      end
+    end
+  end
+  return minkey
+
 end
